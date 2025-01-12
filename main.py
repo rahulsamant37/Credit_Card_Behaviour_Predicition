f92c31648df97edfdeae0b4d6e2e1cd5961e449f
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2025-01-12 06:08:13 +0000

    Initializing main.py for data validation

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from src import logger
 from src.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.inititate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.inititate_data_validation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
