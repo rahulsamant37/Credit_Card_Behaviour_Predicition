7fe04ff583fad584f20b19aea6efa9a38ac210a0
rahulsamant37 <rahulsamantcoc2gmail.com>
2025-01-13 15:31:40 +0530

    Initializing main.py for data tranformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src import logger
 from src.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -17,6 +18,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.inititate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.inititate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
